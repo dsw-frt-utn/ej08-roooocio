00c2fc29f046e45ddd2599585f0a8c913403215f
Rocio <Rocio.ovejero@alu.frt.utn.edu.ar>
2026-05-04 22:16:28 -0300

    feat: problema 5 terminado

---
+++ Dsw2026Ej8/Problema 5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problema5 p5 = new Problema5();
+            RetailSale v1 = new RetailSale { Importe = 1000m };
+            WholesaleSale v2 = new WholesaleSale { Importe = 1000m };
+            Console.WriteLine($"Venta Minorista: {p5.ObtenerImporteFinal(v1)}"); 
+            Console.WriteLine($"Venta Mayorista (10% desc): {p5.ObtenerImporteFinal(v2)}"); 
+
+        }
+    }
+}
---
+++ Dsw2026Ej8/Problema 5/Sale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_5
+{
+    public class Sale
+    {
+        public decimal Importe { get; set; }
+        public virtual decimal CalculateTotal()
+        {
+            return Importe;
+        }
+
+    }
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Importe * 0.9m;
+        }
+    }
+    
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return Importe; 
+        }
+    }
+}

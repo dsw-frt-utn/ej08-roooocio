fab35fddda595f10d793dab7f1318f1ff47b8dae
Rocio <Rocio.ovejero@alu.frt.utn.edu.ar>
2026-05-04 21:26:39 -0300

    feat: resolucion del problema 2

---
+++ Problema2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Ej08Problema2
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal totalCalculado = quantity > 0 ? (quantity * unitPrice) : 0;
+
+            var resumen = new //clase anonima
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total:C}";
+         }
+
+    }
+
+         
+}
+    
+
---
+++ Program (2).cs
@@ -0,0 +1,11 @@
+﻿namespace Ej08Problema2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Problema2 prob2 = new Problema2();
+            Console.WriteLine(prob2.CrearResumenVenta(1000, "Gasesosa", 2, 15.0m));
+        }
+    }
+}

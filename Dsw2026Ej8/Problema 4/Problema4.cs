677ecdf2ec38ee0582715b0a7aa975c989db428a
Rocio <Rocio.ovejero@alu.frt.utn.edu.ar>
2026-05-04 22:03:08 -0300

    feat: problema 4 terminado

---
+++ Dsw2026Ej8/Problema 4/Problema4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_4
+{
+    public class Problema4
+    {
+     public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            double suma = 0;
+            int cantidadValidas = 0;
+            int?[] notas = { nota1, nota2, nota3 };
+
+            foreach (var nota in notas)
+            {
+                if (nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    suma += nota.Value;
+                    cantidadValidas++;
+                }
+            }
+
+            if (cantidadValidas == 0)
+            {
+                return 0;
+            }
+
+            return suma / cantidadValidas;
+        }
+
+    }
+}
---
+++ Dsw2026Ej8/Problema 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problema4 p4 = new Problema4();
+            double prom1 = p4.CalcularPromedio(8, null, 10);
+            Console.WriteLine($"Promedio 8, null, 10: {prom1}"); 
+            double prom2 = p4.CalcularPromedio(null, null, null);
+            Console.WriteLine($"Promedio todos null: {prom2}");
+            double prom3 = p4.CalcularPromedio(7, 20, -5);
+            Console.WriteLine($"Promedio 7, 20, -5: {prom3}"); 
+        }
+    }
+}
